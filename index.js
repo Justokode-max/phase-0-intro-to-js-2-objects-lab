5017e132321e2d516a525a76c20d48f10960bef1
Justus Dianga Okode <justusdiangaokode@gmail.com>
2022-11-22 10:26:59 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {name:"Justus", streetAddress:"Ongata"};
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    const newEmployee={...employee,name:"Sam",streetAddress:"11 Broadway"};
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+   employee.name="Sam";
+   employee.streetAddress="12 Broadway"
+   return employee;
+
+}
+function deleteFromEmployeeByKey(employee,key){
+    const Employee={...employee};
+    delete Employee.name;
+    return Employee;
+    
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee.name;
+    return employee;
+}
